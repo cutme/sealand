07008a1ab693bbab60a3f54ded6b54bb1c20c21c
Bartosz Mediger <cutme@users.noreply.github.com>
2020-03-09 12:13:07 +0100

    Js fix

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require('./js/gototarget.js');
 require('./js/map.js');
 require('./js/nav.js');
 //require('./js/splitting.js');
-require('./js/homevideo.js');
+//require('./js/homevideo.js');
 require('./js/background-video.js');
 
 
